1530a9f8f9443a123f099e8c444427a41ceb1609
marykasp <kasparian.mary@gmail.com>
2023-11-09 14:20:43 -0600

    chore: install and setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#ff77e9",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
