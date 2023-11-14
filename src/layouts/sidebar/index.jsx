c9f85c71706e53b4cfe5ad2769749c7f193761cf
marykasp <kasparian.mary@gmail.com>
2023-11-14 11:12:56 -0600

    style: create logo component at top of sidebar

--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -38,12 +38,29 @@ const Sidebar = () => {
         animate={isOpen ? "open" : "closed"}
         className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"
       >
-        <div
+        <div className="flex items-center gap-2.5 font-medium border-b border-slate-300 py-3 mx-3">
+          <img
+            src="https://img.icons8.com/color/512/firebase.png"
+            alt=""
+            width={45}
+          />
+          <span className="text-xl whitespace-pre">Fireball</span>
+        </div>
+        {/* control button */}
+        <motion.div
+          animate={
+            isOpen
+              ? { x: 0, y: 0, rotate: 0 }
+              : { x: -10, y: -200, rotate: 180 }
+          }
+          transition={{
+            duration: 0,
+          }}
           onClick={() => setIsOpen(!isOpen)}
-          className="absolute w-fit z-50 right-2 bottom-5 cursor-pointer"
+          className="absolute w-fit z-50 right-2 bottom-5 cursor-pointer md:block hidden"
         >
           <IoIosArrowBack size={25} />
-        </div>
+        </motion.div>
       </motion.div>
     </div>
   );
